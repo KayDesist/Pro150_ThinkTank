5f9f08dcbe3d9237696fb146ca5b6bd883c47358
crazyman222214 <schadejr617@gmail.com>
2025-02-18 10:17:12 -0700

    Initial DB Model Setup

---
+++ BadEnglishGames/BadEnglishGames/Data/Models/Game.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace BadEnglishGames.Data.Models
+{
+    public class Game
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+        public int? GameID { get; set; } = null!;
+        public string? GameTitle { get; set; }
+        public string? GameDesc { get; set; }
+    }
+}
