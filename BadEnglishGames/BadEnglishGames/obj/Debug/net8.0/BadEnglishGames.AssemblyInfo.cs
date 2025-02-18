e056f9b948b2626e0002175b81ca839174c6299d
Nyckolas12 <nmendez@student.neumont.edu>
2025-02-18 10:18:11 -0700

    testing

--- BadEnglishGames/BadEnglishGames/obj/Debug/net8.0/BadEnglishGames.AssemblyInfo.cs
+++ BadEnglishGames/BadEnglishGames/obj/Debug/net8.0/BadEnglishGames.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BadEnglishGames")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9037c5547c74ef76b2b65a45915d3062ef5a3d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abd0f83d32a66df7cce7a2f88101bc340959f909")]
 [assembly: System.Reflection.AssemblyProductAttribute("BadEnglishGames")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BadEnglishGames")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
