e4e1c386fee9800d87f84081b59d7fe4a90c6da5
Hawkeye9157 <157405016+Hawkeye9157@users.noreply.github.com>
2025-02-19 10:05:49 -0700

    Added user structure

---
+++ BadEnglishGames/BadEnglishGames/Data/Models/User.cs
@@ -0,0 +1,20 @@
+﻿namespace BadEnglishGames.Data.Models
+{
+    public class User
+    {
+        //account creation
+        public string Username { get; set; }
+        public string Email { get; set; }
+        //only used for creation, cleared afterward
+        public string Password { get; set; }
+
+        //saved scores
+        public int GameHS { get; set; }
+
+        //called after storing user in db
+        public void ClearPassword()
+        {
+
+        }
+    }
+}
--- BadEnglishGames/BadEnglishGames/Pages/Register.cshtml.cs
+++ BadEnglishGames/BadEnglishGames/Pages/Register.cshtml.cs
@@ -1,21 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using BadEnglishGames.Data.Models;
 
 namespace BadEnglishGames.Pages
 {
     public class RegisterModel : PageModel
     {
+        public User temp;
+        public string confirm { get; set; }
         public void OnGet()
         {
         }
         private IActionResult OnPostSignUp()
         {
+            if (temp == null) return RedirectToAction("Get");
+            //check if username and email are unique
 
+            if (temp.Password != confirm) return RedirectToAction("Get");
+
+            //add user to db
+
+            temp.ClearPassword();
+            temp = new User();
+            confirm = "";
             return RedirectToAction("Index");
-        }
-        private IActionResult OnPostLogin()
-        {
-            return RedirectToAction("Login");
         }
     }
 }
