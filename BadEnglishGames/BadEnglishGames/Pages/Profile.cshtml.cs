9e707ed35435cc738b196b925b08e5350a03b0d4
Nyckolas12 <nmendez@student.neumont.edu>
2025-02-18 19:35:16 -0700

    UI Displays
    Added displays for Username and Description

--- BadEnglishGames/BadEnglishGames/Pages/Profile.cshtml.cs
+++ BadEnglishGames/BadEnglishGames/Pages/Profile.cshtml.cs
@@ -5,6 +5,10 @@ namespace BadEnglishGames.Pages
 {
     public class ProfileModel : PageModel
     {
+        public string UserName { get; set; } = "FunnyUserName";
+        public string UserDescription { get; set; }
+
+
         public void OnGet()
         {
         }
--- BadEnglishGames/BadEnglishGames/obj/Debug/net8.0/BadEnglishGames.AssemblyInfo.cs
+++ BadEnglishGames/BadEnglishGames/obj/Debug/net8.0/BadEnglishGames.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BadEnglishGames")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abd0f83d32a66df7cce7a2f88101bc340959f909")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e056f9b948b2626e0002175b81ca839174c6299d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BadEnglishGames")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BadEnglishGames")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
