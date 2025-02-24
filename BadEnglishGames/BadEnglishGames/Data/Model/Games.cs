bb9da4f88515c55b28bb5ffe06589a554e9c8db1
KayDesist <desistkayla5@gmail.com>
2025-02-24 09:48:24 -0700

    inti=9?

--- BadEnglishGames/BadEnglishGames/Data/Model/Games.cs
+++ BadEnglishGames/BadEnglishGames/Data/Model/Games.cs
@@ -3,8 +3,8 @@ namespace BadEnglishGames.Models
 {
     public class Game
     {
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public string Route { get; set; } // This will be used to route to the game page
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public string? Route { get; set; } // This will be used to route to the game page
     }
 }
