e29f5938157ea5c64a354e32f5d9ba90cc0a4599
KayDesist <desistkayla5@gmail.com>
2025-02-27 10:08:10 -0700

    Chris please

--- BadEnglishGames/BadEnglishGames/Pages/Login.cshtml.cs
+++ BadEnglishGames/BadEnglishGames/Pages/Login.cshtml.cs
@@ -1,3 +1,5 @@
+using BadEnglishGames.Data.DAL;
+using BadEnglishGames.Data.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,44 @@ namespace BadEnglishGames.Pages
 {
     public class LoginModel : PageModel
     {
+        public bool? ValidLogin { get; set; } = null;
+        public string? LoginPopupText { get; set; } = null;
+
         public void OnGet()
         {
+        }
+        public void OnPostLogin()
+        {
+            string? username = Request.Form["LoginUsername"];
+            string? password = Request.Form["LoginPassword"];
+
+            //Check if the username is in the DB first
+            if (username != null && password != null)
+            {
+                ValidLogin = ValidateLogin(username, password);
+            }
+        }
+
+        private bool ValidateLogin(string username, string password)
+        {
+            bool validLogin = false;
+
+            //The user will be null if the username is not in the DB
+            User? user = DatabaseController.GetUserByUsername(username);
+
+            if (user != null)
+            {
+                //After validating the username exists, you can verify that the given password is the correct password
+                validLogin = user.password == password;
+            }
+            else
+            {
+                validLogin = false;
+                
+            }
+
+            LoginPopupText = (validLogin) ? "" : "The Username And/Or Password is incorrect. Try Again";
+            return validLogin;
         }
     }
 }
--- BadEnglishGames/BadEnglishGames/Pages/Register.cshtml.cs
+++ BadEnglishGames/BadEnglishGames/Pages/Register.cshtml.cs
@@ -1,12 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using BadEnglishGames.Data.Models;
 
 namespace BadEnglishGames.Pages
 {
     public class RegisterModel : PageModel
     {
+        public User temp;
+        public string confirm { get; set; }
         public void OnGet()
         {
+        }
+        public IActionResult OnPostSignUp()
+        {
+            if (temp == null) return RedirectToAction("Get");
+            //check if username and email are unique
+
+            if (temp.password != confirm) return RedirectToAction("Get");
+
+            //add user to db
+            bool success = Data.DAL.DatabaseController.CreateUser(temp);
+            if (!success)
+            {
+                RedirectToAction("Get");
+            }
+            return RedirectToAction("Index");
         }
     }
 }
