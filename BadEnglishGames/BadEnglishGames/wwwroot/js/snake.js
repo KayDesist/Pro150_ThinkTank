eaf40cb570b794dc708018715f3cb758ba0c4993
crazyman222214 <schadejr617@gmail.com>
2025-03-01 15:19:21 -0700

    Completed Snake

--- BadEnglishGames/BadEnglishGames/wwwroot/js/snake.js
+++ BadEnglishGames/BadEnglishGames/wwwroot/js/snake.js
@@ -7,19 +7,79 @@ class Snake {
     direction = [0, 0];
     body = [[0, 0]];
 
+    growSnake() {
+        //If the snake has only one segment to the body,
+        //add the new segment to the end in the opposite direction of the movement (direction var)
+
+        var newPosition = [];
+
+        if (this.body.length == 1) {
+            //Getting the direction opposite of our movement
+            var newDirection = [this.direction[0] * -1, this.direction[1] * -1];
+
+            //Getting the position from the position plus newDirection
+            newPosition = [this.position[0] + newDirection[0], this.position[1] + newDirection[1]];
+        }
+
+        //We need the last position and the second last position of the snake
+        //This way we can gauge the "endDirection" of the tail
+        else if (this.body.length > 1) {
+            //Formula: (SecondTail = ST) (Tail = T) (N = new)
+            //   ST - T = N_dir
+            //   -N_dir + T = N_pos
+
+            var secondTailPosition = this.body[this.body.length - 2];
+            var tailPosition = this.body[this.body.length - 1];
+
+            
+            var newDirection = [secondTailPosition[0] - tailPosition[0], secondTailPosition[1] - tailPosition[1]];
+            var negNewDirection = [newDirection[0] * -1, newDirection[1] * -1];
+
+            newPosition = [negNewDirection[0] + tailPosition[0], negNewDirection[1] + tailPosition[1]];
+        }
+
+        this.body.push(newPosition);
+        //
+
+    }
+
+
     updatePosition() {
 
         this.x += this.direction[0];
         this.y += this.direction[1];
         this.position = [this.x, this.y];
+        //The last segment of the body becomes the first part of the body
 
-        // Move the snake
-        this.body.pop(); 
+        //Unless the length is one, in which case we just update the head
+        if (this.body.length == 1) {
+            this.body[0] = this.position;
+        }
+        else if (this.body.length > 1) {
+            var lastPosition = this.body.pop();
+
+            //Adds the last position to the front of the snake
+            this.body.unshift(this.position);
+        } 
         
     }
 
     checkCollision() {
-        return this.body.slice(1).some(segment => segment[0] === this.x && segment[1] === this.y);
+        var inBody = false;
+        
+        for (var pos of this.body.slice(1)) {
+            if (pos[0] == this.position[0] && pos[1] == this.position[1]) {
+                inBody = true;
+            }
+        }
+
+        return inBody;
+    }
+
+    drawSnake() {
+        for (var pos of this.body) {
+            if (pos != null) document.getElementById(pos[0] + " - " + pos[1]).style.backgroundColor = "green";
+        }
     }
 
     constructor(x, y) {
@@ -42,6 +102,10 @@ class Apple {
         this.x = Math.floor(Math.random() * 12);
         this.y = Math.floor(Math.random() * 30);
     }
+
+    drawApple() {
+        document.getElementById(this.x + " - " + this.y).style.backgroundColor = "red";
+    }
 }
 
 class Game {
@@ -51,27 +115,81 @@ class Game {
     screenWidth = 30;
     screenHeight = 12;
 
-    constructor() {
+    score = 0;
+
+    startGame() {
         this.snake = new Snake(1, 1);
         this.apple = new Apple();
     }
 
+    constructor() {
+        this.startGame();
+    }
+
+    gameOver() {
+        alert("Game Over! Score: " + this.score);
+        this.startGame();
+    }
+
+    update() {
+        this.snake.updatePosition();
+        this.snakeCollision();
+        this.screenCollision();
+        this.appleCollision();
+        this.clearBoard();
+        this.snake.drawSnake();
+        this.apple.drawApple();
+    }
+
     screenCollision() {
         if (this.snake.x < 0 || this.snake.x >= 12 || this.snake.y < 0 || this.snake.y >= 30) {
-            alert("Game Over! Score: " + this.score);
-            location.reload(); // Reload to restart game
+            this.gameOver();
+        }
+    }
+
+    snakeCollision() {
+        if (this.snake.checkCollision()) {
+            this.gameOver();
+        }
+    }
+
+    spawnApple() {
+        this.apple = new Apple();
+
+        var isInSnake = false;
+
+        while (!isInSnake) {
+            this.apple.setSpawnLocation();
+            isInSnake = !this.snake.body.includes(this.apple.position);
         }
     }
 
     appleCollision() {
         if (this.snake.x === this.apple.x && this.snake.y === this.apple.y) {
-            this.score++;
-            spawnApple();
 
-            newPosition = [this.snake.body[this]]
+            //Getting score updated
+            this.score += 10;
 
-            this.body.push(); // Grow the snake
+            //Spawning new apple
+            this.spawnApple();
+
+            //Grow Snake
+            this.snake.growSnake();
+
         }
+    }
+
+    clearBoard() {
+
+        var cells = document.getElementsByClassName("SnakeCell");
+
+        for (let rows = 0; rows < 12; rows++) {
+            for (let cells = 0; cells < 30; cells++) {
+                document.getElementById(rows + " - " + cells).style.backgroundColor = "white";
+            }
+        }
+
+
     }
 }
 
@@ -83,47 +201,23 @@ document.onkeypress = (e) => {
     let key = e.keyCode;
 
     if (key == "w".charCodeAt(0)) {
-        game.snake.direction = [0, -1];
-    }
-    else if (key == "a".charCodeAt(0)) {
         game.snake.direction = [-1, 0];
     }
-    else if (key == "s".charCodeAt(0)) {z
-        game.snake.direction = [0, 1];
+    else if (key == "a".charCodeAt(0)) {
+        game.snake.direction = [0, -1];
+    }
+    else if (key == "s".charCodeAt(0)) {
+        game.snake.direction = [1, 0];
     }
     else if (key == "d".charCodeAt(0)) {
-        game.snake.direction = [1, 0];
+        game.snake.direction = [0, 1];
     }
 }
 
 //Setting up Game Tick
 function tick() {
-    game.snake.updatePosition();
-    clearBoard();
-    drawSnake();
-    drawApple();
-    document.getElementById("test").innerHTML = game.snake.position;
+    game.update();
     //document.getElementById("test").innerHTML = "Score: " + snake.score;
-}
-
-function clearBoard() {
-    var cells = document.getElementsByClassName("SnakeCell");
-
-    for (let rows = 0; rows < 12; rows++) {
-        for (let cells = 0; cells < 30; cells++) {
-            document.getElementById(rows + " - " + cells).style.backgroundColor = "white";
-        }
-    }
-
-    
-}
-
-function drawSnake() {
-    document.getElementById(game.snake.x + " - " + game.snake.y).style.backgroundColor = "green";
-}
-
-function drawApple() {
-    document.getElementById(game.apple.x + " - " + game.apple.y).style.backgroundColor = "red";
 }
 
 setInterval(tick, 100);
